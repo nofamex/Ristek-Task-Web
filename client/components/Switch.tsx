1840e33ece102bafea617cb280e615100a264551
nofamex <nsiren517@gmail.com>
2021-03-22 01:42:10 +0700

    fix: redefine condition in switching

--- client/components/Switch.tsx
+++ client/components/Switch.tsx
@@ -4,27 +4,19 @@ import { FaMoon } from 'react-icons/fa';
 import { HiSun } from 'react-icons/hi';
 import { IconContext } from 'react-icons';
 
-function Switch({ isOn, ...props }) {
-  const rounded = tw`w-6 h-6 bg-primary dark:bg-white rounded-full flex justify-center items-center`;
-  const toggler = (isOn) => [
-    tw`w-12 h-4 rounded-full flex cursor-pointer items-center`,
-    isOn
-      ? tw`justify-end bg-gray-300 transition-all`
-      : tw`justify-start bg-gray-300 transition-all`,
-  ];
+const rounded = tw`w-6 h-6 bg-primary dark:bg-white rounded-full flex justify-center items-center`;
+const toggler = (isOn) => [
+  tw`w-12 h-4 rounded-full flex cursor-pointer items-center`,
+  isOn ? tw`justify-end bg-gray-300 transition-all` : tw`justify-start bg-gray-300 transition-all`,
+];
 
+function Switch({ isOn, ...props }) {
   return (
     <motion.div layout css={toggler(isOn)} {...props}>
       <motion.div layout css={rounded}>
-        {isOn ? (
-          <IconContext.Provider value={{ color: '#43379d', size: '1.2em' }}>
-            <FaMoon />
-          </IconContext.Provider>
-        ) : (
-          <IconContext.Provider value={{ color: 'white', size: '1.2em' }}>
-            <HiSun />
-          </IconContext.Provider>
-        )}
+        <IconContext.Provider value={{ color: isOn ? '#43379d' : 'white', size: '1.2em' }}>
+          {isOn ? <FaMoon /> : <HiSun />}
+        </IconContext.Provider>
       </motion.div>
     </motion.div>
   );
