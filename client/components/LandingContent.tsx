6bdda43b567814ac0291ce854fff1295be039f69
nofamex <nsiren517@gmail.com>
2021-03-27 03:31:43 +0700

    fix: fix __next styling

--- client/components/LandingContent.tsx
+++ client/components/LandingContent.tsx
@@ -2,14 +2,15 @@ import tw from 'twin.macro';
 import { motion } from 'framer-motion';
 
 const LandingText = tw`font-logoFont text-primary text-center text-8xl md:text-9xl cursor-pointer dark:text-white`;
+const LandingContainer = tw.div`height[85vh] relative z-10 flex flex-col justify-center overflow-hidden`;
 
 function LandingContent() {
   return (
-    <div tw="w-full h-full flex-grow relative z-10 flex flex-col justify-center overflow-hidden">
+    <LandingContainer>
       <motion.div css={LandingText} drag={true} whileDrag={{ scale: 1.1 }}>
         Frickster
       </motion.div>
-    </div>
+    </LandingContainer>
   );
 }
 
